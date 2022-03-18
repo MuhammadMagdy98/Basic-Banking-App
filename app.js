7074bad98dbfb95d08419e0d9ff3bfdf3e740520
Muhammad Magdy <lukecholes@gmail.com>
2022-03-18 16:14:21 +0200

    added extra validations

--- app.js
+++ app.js
@@ -63,42 +63,140 @@ app.get("/transactionHisory", (req, res) => {
 });
 
 function validateBalance(balance) {
+  return new Promise((resolve, reject) => {
+    if (balance.length === 0) {
+      resolve({ valid: false, msg: `Balance can't be empty` });
+    } else if (!(Number(balance) == balance)) {
+      resolve({ valid: false, msg: `Balance is not a number` });
+    } else if (balance % 1 != 0) {
+      resolve({ valid: false, msg: `Balance should be an integer` });
+    } else if (balance <= 0) {
+      resolve({
+        valid: false,
+        msg: `Balance must be a positive integer`,
+      });
+    }
 
-  if (balance.length === 0) {
-    return { validBalance: false, msg: `Invalid Balance` };
-  }
-  if (!(Number(balance) == balance)) {
-    return { validBalance: false, msg: `Balance is not a number` };
-  }
-
-  if (balance % 1 != 0) {
-    return { validBalance: false, msg: `Balance should be an integer` };
-  }
-
-  if (balance <= 0) {
-    return { validBalance: false, msg: `Balance must be a positive integer` };
-  }
-
-  return { validBalance: true, msg: "" };
+    resolve({ valid: true, msg: "" });
+  });
 }
 
+function customerExist(customer) {
+  return new Promise((resolve, reject) => {
+    con.query(
+      `select * from customers where name='${customer}'`,
+      (err, result, fields) => {
+        if (err) throw err;
+        result = JSON.parse(JSON.stringify(result));
+
+        if (result.length === 0) {
+          resolve({ valid: false, msg: `Invalid Data` });
+        } else {
+          resolve({ valid: true, msg: "" });
+        }
+      }
+    );
+  });
+}
 function validateCustomer(customer) {
-  if (customer.length === 0) {
-    return { validCustomer: false, msg: `Invalid Data` };
-  }
-
-  for (let i = 0; i < customer.length; i++) {
-    let code = customer.charCodeAt(i);
-    if (
-      !(code > 47 && code < 58) && // numeric (0-9)
-      !(code > 64 && code < 91) && // upper alpha (A-Z)
-      !(code > 96 && code < 123) // lower alpha (a-z)
-    ) {
-      return { validCustomer: false, msg: `Invalid Data` };
+  return new Promise((resolve, reject) => {
+    if (customer.length === 0) {
+      resolve({ valid: false, msg: `Invalid Data` });
     }
+
+    for (let i = 0; i < customer.length; i++) {
+      let code = customer.charCodeAt(i);
+      if (
+        !(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123) // lower alpha (a-z)
+      ) {
+        resolve({ valid: false, msg: `Invalid Data` });
+      }
+    }
+
+    resolve({ valid: true, msg: `Invalid Data` });
+  });
+}
+
+async function updateCustomer(fromName, toName, balance) {
+  return new Promise((resolve, reject) => {
+    con.query(
+      `select * from customers where name='${fromName}'`,
+      (err, result, fields) => {
+        if (err) throw err;
+
+        result = JSON.parse(JSON.stringify(result));
+
+        const cust = result[0];
+
+        const remainingBalance = cust.balance - balance;
+        if (remainingBalance < 0) {
+          resolve({ valid: false, msg: `You don't have enough balance.` });
+        } else {
+          con.query(
+            `update customers set balance=${remainingBalance} where name='${fromName}'`,
+            (err, result) => {
+              let currentDate = new Date().toLocaleString();
+              console.log(currentDate);
+              con.query(
+                `insert into transaction_history (sender_name, receiver_name, transaction_time, balance)
+        values ('${fromName}', '${toName}', '${currentDate}', ${balance})`,
+                (err, result) => {
+                  if (err) throw err;
+                  console.log("date updated");
+                }
+              );
+              if (err) throw err;
+
+              con.query(
+                `update customers set balance = balance + ${balance} where name='${toName}'`,
+                (err, result) => {
+                  if (err) throw err;
+                  resolve({ valid: true, msg: "Transaction is successful" });
+                }
+              );
+            }
+          );
+        }
+      }
+    );
+  });
+}
+async function handlePromises(fromName, toName, balance) {
+  let res = await validateBalance(balance);
+
+  if (!res.valid) {
+    return res;
   }
 
-  return { validCustomer: true, msg: "" };
+  res = await validateCustomer(fromName);
+
+  if (!res.valid) {
+    return res;
+  }
+
+  res = await customerExist(fromName);
+
+  if (!res.valid) {
+    return res;
+  }
+
+  res = await validateCustomer(toName);
+
+  if (!res.valid) {
+    return res;
+  }
+
+  res = await customerExist(toName);
+
+  if (!res.valid) {
+    return res;
+  }
+
+  res = await updateCustomer(fromName, toName, balance);
+
+  return res;
 }
 app.post("/send", (req, res) => {
   let fromName = req.body.from;
@@ -107,65 +205,12 @@ app.post("/send", (req, res) => {
 
   fromName = fromName.split(" ")[0];
   toName = toName.split(" ")[0];
-  {
-    let { validBalance, msg } = validateBalance(balance);
-    if (!validBalance) {
-      return res.status(200).send(msg);
-    }
-  }
-  {
-    let { validCustomer, msg } = validateCustomer(fromName);
 
-    if (!validCustomer) {
-      return res.status(200).send(msg);
-    }
-  }
+  handlePromises(fromName, toName, balance).then((result) => {
+    res.status(200).send(result.msg);
+  });
 
-  let { validCustomer, msg } = validateCustomer(toName);
-  if (!validCustomer) {
-    return res.status(200).send(msg);
-  }
-
-  con.query(
-    `select * from customers where name='${fromName}'`,
-    (err, result, fields) => {
-      if (err) throw err;
-
-      result = JSON.parse(JSON.stringify(result));
-
-      const cust = result[0];
-
-      const remainingBalance = cust.balance - balance;
-      if (remainingBalance < 0) {
-        res.status(416).send(`You don't have enough balance.`);
-      } else {
-        con.query(
-          `update customers set balance=${remainingBalance} where name='${fromName}'`,
-          (err, result) => {
-            let currentDate = new Date().toLocaleString();
-            console.log(currentDate);
-            con.query(
-              `insert into transaction_history (sender_name, receiver_name, transaction_time, balance)
-        values ('${fromName}', '${toName}', '${currentDate}', ${balance})`,
-              (err, result) => {
-                if (err) throw err;
-                console.log("date updated");
-              }
-            );
-            if (err) throw err;
-
-            con.query(
-              `update customers set balance = balance + ${balance} where name='${toName}'`,
-              (err, result) => {
-                if (err) throw err;
-                res.status(200).send("Transaction is successful");
-              }
-            );
-          }
-        );
-      }
-    }
-  );
+  // console.log(result);
 });
 con.connect(function (err) {
   if (err) throw err;
